1a2c3e3a4bdb0bb90518284a9632d3c0d1d51b24
Kirk Yuan <yuanjt810@gmail.com>
2018-04-06 15:43:23 -0400

    MUBARIS THE OWNER TEXT NEEDS TO BE BIGGER

--- src/App.js
+++ src/App.js
@@ -3,28 +3,6 @@ import styled, { injectGlobal } from 'styled-components';
 import { Line } from 'rc-progress';
 import moment from 'moment';
 
-const gradients = [
-  'background:linear-gradient(135deg, #CE9FFC 0%,#7367F0 100%);',
-  'background:linear-gradient(135deg, #b1ea4d 0%,#459522 100%);',
-  'background:linear-gradient(135deg, #c3ec52 0%,#0ba29d 100%);',
-  'background:linear-gradient(135deg, #0FF0B3 0%,#036ED9 100%);',
-  'background:linear-gradient(135deg, #13f1fc 0%,#0470dc 100%);',
-  'background:linear-gradient(135deg, #C56CD6 0%,#3425AF 100%);',
-  'background:linear-gradient(135deg, #F36265 0%,#961276 100%);',
-  'background:linear-gradient(135deg, #F5515F 0%,#A1051D 100%);',
-  'background:linear-gradient(135deg, #f2d50f 0%,#da0641 100%);',
-  'background:linear-gradient(135deg, #fad961 0%,#f76b1c 100%);',
-  'background:linear-gradient(135deg, #5b247a 0%,#1bcedf 100%);',
-  'background:linear-gradient(135deg, #184e68 0%,#57ca85 100%);',
-  'background:linear-gradient(135deg, #65799b 0%,#5e2563 100%);',
-  'background:linear-gradient(135deg, #f02fc2 0%,#6094ea 100%);',
-  'background:linear-gradient(135deg, #7117ea 0%,#ea6060 100%);',
-  'background:linear-gradient(135deg, #622774 0%,#c53364 100%);',
-  'background:linear-gradient(135deg, #17ead9 0%,#6078ea 100%);',
-  'background:linear-gradient(135deg, #f65599 0%,#4d0316 100%);',
-  'background:linear-gradient(135deg, #fcdf8a 0%,#f38381 100%);'
-];
-
 const gradient = 'background-image: url("https://source.unsplash.com/collection/176316/1650x1080")';
 
 
@@ -65,13 +43,14 @@ const DateDiv = styled.div`
 const AuthorDiv = styled.div`
   display: flex;
   position: absolute;
-  left: 81%;
+  left: 85%;
   right: 0%;
   bottom: 0;
   margin: 0 1% 1% 0;
   width: 18%;
   color: #ffffff;
   a {
+    font-size: 0.8rem;
     color: #ffffff;
     text-decoration: none;
   }
@@ -80,6 +59,7 @@ const AuthorDiv = styled.div`
 const RealAuthorDiv = styled.div`
   color: #ffffff;
   a {
+    font-size: 1rem;
     color: #ffffff;
     text-decoration: none;
   }
@@ -149,7 +129,7 @@ class App extends Component {
           </div>
         </DateDiv>
         <RealAuthorDiv>
-          <a href="https://twitter.com/Mubaris_NK" target="_blank" rel="noopener noreferrer">Made with ♥ by Mubaris NK</a>
+          <a href="https://twitter.com/Mubaris_NK" target="_blank" rel="noopener noreferrer">Source Code made with ♥ by Mubaris NK</a>
         </RealAuthorDiv>
         <AuthorDiv>
         <a href="https://twitter.com/kirkyuan" target="_blank" rel="noopener noreferrer">Customized with ♥ by Kirk Yuan</a>
